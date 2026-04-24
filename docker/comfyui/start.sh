a99a458a519877da7e3a5ee86f0485ebb20cbec2
freshmea <freshmea@gmail.com>
2026-04-24 21:07:02 +0900

    workflow

--- docker/comfyui/start.sh
+++ docker/comfyui/start.sh
@@ -13,4 +13,5 @@ cd /opt/comfyui
 exec python main.py \
   --listen "${COMFYUI_LISTEN:-0.0.0.0}" \
   --port "${COMFYUI_INTERNAL_PORT:-8188}" \
-  --disable-auto-launch
+  --disable-auto-launch \
+  --enable-manager
